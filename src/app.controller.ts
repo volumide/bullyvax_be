9860e9d7217dce2d794bc3d3135cb19f34726db5
Afowowe Olumide Victor <volumide42@gmail.com>
2022-03-16 14:18:01 +0100

    sponsorship restored

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -45,6 +45,7 @@ export class AppController {
     // @Param('id') id: boolean,
     @Query('zip_name') zip_name?: string,
   ): Promise<SponsorshipDto[]> {
+    if (zip_name) return this.appService.getSponsorships(zip_name, 'true');
     return this.appService.getSponsorships(zip_name);
   }
 
